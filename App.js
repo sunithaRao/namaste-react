35ddb145930d91473da4f56063fc813cc7c639c4
sunitha <suni.lucia@gmail.com>
2024-10-12 12:05:12 +0530

    intial file with hello world react example

---
+++ App.js
@@ -0,0 +1,14 @@
+const parent = React.createElement('div', {id:'parent'}, [
+    React.createElement('div', {id:'child1'}, [
+      React.createElement('h1',{}, 'I m from h1 tag'),
+      React.createElement('h2',{}, 'I m from h2 tag')
+   ]),
+   React.createElement('div', {id:'child2'}, [
+      React.createElement('h1',{}, 'I m from h1 tag'),
+      React.createElement('h2',{}, 'I m from h2 tag')
+   ])
+
+]);
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(parent);
